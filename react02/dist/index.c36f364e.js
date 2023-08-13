f0f51216e6a0554fa97615cb289168c294f695ae
DikshaChauhan18 <dikshachauhan187@gmail.com>
2023-08-13 22:47:58 +0530

    first react app- printing hello world

---
+++ react02/dist/index.c36f364e.js
@@ -0,0 +1,5 @@
+const root = ReactDOM.createRoot(document.getElementById("root"));
+const heading = React.createElement("h1", {}, "hello every one !!");
+root.render(heading);
+
+//# sourceMappingURL=index.c36f364e.js.map
